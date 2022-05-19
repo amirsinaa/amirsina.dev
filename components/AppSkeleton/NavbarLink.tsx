2dd9a9cf0d27ded378ff127745210cdd88574fbf
amirsina <amirsina23@gmail.com>
2022-05-19 18:14:45 +0430

    - refactor: Create a new separate component for navbar links.

---
+++ components/AppSkeleton/NavbarLink.tsx
@@ -0,0 +1,26 @@
+import {
+  Link,
+  useColorModeValue
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+import { NavbarLinkProps } from "@/types/NavbarLinkProps"
+
+export default function NavbarLink (props: NavbarLinkProps) {
+  return (
+    <NextLink href={props.path} passHref>
+      <Link
+        px={3}
+        py={1.5}
+        rounded={"md"}
+        _hover={{
+          textDecoration: "none",
+          bg: useColorModeValue("gray.200", "gray.900"),
+          color: useColorModeValue("darkgreen.100", "darkgreen.100")
+        }}
+        onClick={() => props.onClose()}
+      >
+        {props.name}
+      </Link>
+    </NextLink>
+  );
+};
