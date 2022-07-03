6ccadccec60cd1617e8af907d5e40d06fb881471
amirsina <amirsina23@gmail.com>
2022-07-03 15:03:24 +0430

    - refactor: rewrite projects component based on the new general card component

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,25 +1,47 @@
-import { MotionBox } from "@/components/user-interface-utilities/chakra-factory";
+import {
+  useColorMode,
+  HStack,
+  Link,
+  Tag
+} from "@chakra-ui/react";
+import { assignTechTagColor } from "@/components/user-interface-utilities/theme";
+import Card from "@/components/user-interface-utilities/card";
 import { VStack, SimpleGrid } from "@chakra-ui/react";
 import { LayoutGroup } from "framer-motion";
 import ProjectsProps from "@/types/project";
-import ProjectCard from "./project-card";
-
+import NextLink from 'next/link';
 
 const Projects = ({ projects }: ProjectsProps) => {
+  const { colorMode } = useColorMode();
   return (
     <VStack mx={2} align="start" spacing={8}>
       <LayoutGroup>
         <SimpleGrid columns={1} spacing={4} mt={5} w="100%">
           {projects.map((project) => (
-            <MotionBox whileHover={{ y: -4.5, scale: 1.01 }} key={project.id}>
-              <ProjectCard
-                title={project.title}
-                description={project.description}
-                logo={project.logo}
-                link={project.link}
-                technologies={project.technologies}
-              />
-            </MotionBox>
+            <NextLink href={project.link} key={project.id} passHref>
+                <Link isExternal>
+                  <Card
+                    title={project.title}
+                    description={project.description}
+                    logo={project.logo}
+                    colorMode={colorMode}
+                  >
+                    <HStack pt={2} display={{ sm: "none", md: "flex" }}>
+                      {project.technologies.map((tech) => (
+                        <Tag
+                          key={tech.id}
+                          size="sm"
+                          fontWeight="bold"
+                          m={["0px", "0px", "5px"]}
+                          colorScheme={assignTechTagColor(tech)}
+                        >
+                          {tech.name}
+                        </Tag>
+                      ))}
+                    </HStack>
+                  </Card>
+                </Link>
+              </NextLink>
           ))}
         </SimpleGrid>
       </LayoutGroup>
