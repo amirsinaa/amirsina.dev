416e88dcdfff854adcd6bcd9c2625a362db61ea8
amirsina <amirsina23@gmail.com>
2022-07-03 13:30:38 +0430

    -refactor: replace duplicated code with general card component

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import { MotionBox } from "@/components/UserInterfaceUtilities/ChakraFactory";
+import { MotionBox } from "@/components/user-interface-utilities/chakra-factory";
 import {
   Box,
   Stack,
@@ -6,147 +6,28 @@ import {
   Heading,
   Tag,
   Flex,
-  Text,
   Link,
-  Image,
   List,
   ListItem,
   ListIcon,
   useColorMode,
-  useColorModeValue
 } from "@chakra-ui/react";
 import {
   PageSlideFade,
-  StaggerChildren,
-  CardTransition
-} from "@/components/UserInterfaceUtilities/Transitions";
-import { PublishedArticlesCollectionEnglish } from "@/data/PublishedArticlesCollectionEnglish";
-import { PublishedArticlesCollectionPersian } from "@/data/PublishedArticlesCollectionPersian";
-import { CompaniesCollection } from "@/data/CompaniesCollection";
-import { EducationFacilities } from "@/data/EducationFacilities";
+  StaggerChildren
+} from "@/components/user-interface-utilities/transitions";
+import { EnglishPublishedArticlesData } from "@/data/english-published-articles-data";
+import { PersianPublishedArticlesData } from "@/data/persian-published-articles-data";
+import { CompaniesData } from "@/data/companies-data";
+import { EducationFacilitiesData } from "@/data/education-facilities-data";
 import { BsFillBriefcaseFill, BsStarFill } from "react-icons/bs";
-import PageLayout from "@/components/AppSkeleton/PageLayout";
+import PageLayout from "@/components/skeleton/page-layout";
 import { FaGraduationCap } from "react-icons/fa";
 import { RiArticleLine } from "react-icons/ri";
 import NextLink from 'next/link';
+import Card from "@/components/user-interface-utilities/card";
 
-interface CardProps {
-  title: string;
-  description?: string;
-  role?: string;
-  period?: string;
-  logo?: string;
-  colorMode: string;
-  alt?: string;
-}
-
-const Card = (props: CardProps) => {
-  const { title, role, description, period, logo, colorMode, alt } = props;
-  return (
-    <CardTransition>
-      <Box
-        px={4}
-        py={5}
-        borderWidth="1px"
-        _hover={{ shadow: "lg" }}
-        bg={useColorModeValue("white", "gray.800")}
-        position="relative"
-        rounded="md"
-      >
-        <Flex justifyContent="space-between">
-          <Flex>
-            <Image
-              rounded="full"
-              w={16}
-              h={16}
-              objectFit="cover"
-              fallbackSrc={"/assets/images/image_default_placeholder.png"}
-              src={logo}
-              alt={alt}
-            />
-            <Stack spacing={2} pl={3} align="left">
-              <Heading
-                align="left"
-                fontSize="xl"
-                color={`mode.${colorMode}.career.text`}
-              >
-                {title}
-              </Heading>
-              <Heading
-                align="left"
-                fontSize="sm"
-                color={`mode.${colorMode}.career.subtext`}
-              >
-                {role}
-              </Heading>
-              <Box
-                as="p"
-                textAlign="left"
-                fontSize="sm"
-                color={`mode.${colorMode}.career.subtext`}
-              >
-                {description}
-              </Box>
-              <Box
-                as="p"
-                textAlign="left"
-                fontSize="sm"
-                color={`mode.${colorMode}.career.subtext`}
-              >
-              </Box>
-            </Stack>
-          </Flex>
-          <Stack display={["none", "none", "flex", "flex"]}>
-            <Text fontSize={14} color={`mode.${colorMode}.career.subtext`}>
-              {period}
-            </Text>
-          </Stack>
-        </Flex>
-      </Box>
-    </CardTransition>
-  );
-};
-
-const ArticleCard = (props) => {
-  const { children, articleSource, articleLink, title, lang, colorMode } = props;
-  return (
-    <CardTransition>
-      <Box
-        px={4}
-        py={5}
-        borderWidth="1px"
-        _hover={{ shadow: "lg" }}
-        bg={useColorModeValue("white", "gray.800")}
-        position="relative"
-        rounded="md"
-      >
-        <Flex justifyContent="space-between">
-          <Flex>
-            <Stack spacing={2} pl={3} align="left">
-              <Heading
-                align="left"
-                fontSize="xl"
-                color={`mode.${colorMode}.career.text`}
-              >
-                {title}
-              </Heading>
-              <Box
-                as="p"
-                textAlign="left"
-                fontSize="sm"
-                color={`mode.${colorMode}.career.subtext`}
-              >
-                {children}
-              </Box>
-            </Stack>
-          </Flex>
-        </Flex>
-      </Box>
-    </CardTransition>
-  );
-};
-
-const About = ({ CompaniesCollection, EducationFacilities, PublishedArticlesCollectionEnglish, PublishedArticlesCollectionPersian }) => {
+const About = ({ CompaniesData, EducationFacilitiesData, EnglishPublishedArticlesData, PersianPublishedArticlesData }) => {
   const { colorMode } = useColorMode();
 
   return (
@@ -164,12 +45,12 @@ const About = ({ CompaniesCollection, EducationFacilities, PublishedArticlesColl
             <MotionBox>
               <Heading>
                 <Flex alignItems="center">
-                  <Box as="strong" fontWeight="600">
-                    Career
-                  </Box>
                   <Stack pl={3}>
                     <Box as={BsFillBriefcaseFill} size="25px" />
                   </Stack>
+                  <Box as="strong" fontWeight="600" mx={ 1 }>
+                    Career
+                  </Box>
                 </Flex>
               </Heading>
             </MotionBox>
@@ -180,10 +61,10 @@ const About = ({ CompaniesCollection, EducationFacilities, PublishedArticlesColl
               mx={[0, 0, 3]}
               mt={12}
             >
-              {CompaniesCollection.map((company, index) => (
-                <MotionBox whileHover={{ y: -6, shadow: "lg" }} key={index}>
+              {CompaniesData.map((company) => (
+                <MotionBox whileHover={{ y: -6, shadow: "lg" }} key={company.id}>
                   <Card
-                    key={index}
+                    key={company.id}
                     title={company.title}
                     role={company.role}
                     period={company.period}
@@ -200,10 +81,10 @@ const About = ({ CompaniesCollection, EducationFacilities, PublishedArticlesColl
                     >
                       ACACO is a custom software development and consulting company focusing on web and mobile software development. In this company, many projects have been done in different fields, most of which have been done by the following two main systems, and I have participated in them.
                     </Box>
-                    {company.projects.map((project, index) => (
-                      <MotionBox mt={3} mb={3} whileHover={{ y: -6, shadow: "lg" }} key={index}>
+                    {company.projects.map((project) => (
+                      <MotionBox mt={3} mb={3} whileHover={{ y: -6, shadow: "lg" }} key={project.id}>
                         <Card
-                          key={index}
+                          key={project.id}
                           title={project.title}
                           description={project.description}
                           logo={project.logo}
@@ -266,10 +147,10 @@ const About = ({ CompaniesCollection, EducationFacilities, PublishedArticlesColl
               mx={[0, 0, 6]}
               mt={12}
             >
-              {EducationFacilities.map((institute, index) => (
-                <MotionBox whileHover={{ y: -6, shadow: "lg" }} key={index}>
+              {EducationFacilitiesData.map((institute) => (
+                <MotionBox whileHover={{ y: -6, shadow: "lg" }} key={institute.id}>
                   <Card
-                    key={index}
+                    key={institute.id}
                     title={institute.title}
                     role={institute.role}
                     logo={institute.logo}
@@ -305,16 +186,15 @@ const About = ({ CompaniesCollection, EducationFacilities, PublishedArticlesColl
                 mx={[0, 0, 6]}
                 mt={8}
               >
-                {PublishedArticlesCollectionEnglish.map((enArticleItem, index) => (
-                  <MotionBox whileHover={{ y: -6, shadow: "lg" }} key={index}>
-                    <ArticleCard
-                      key={index}
-                      lang="fa"
+                {EnglishPublishedArticlesData.map((enArticleItem) => (
+                  <MotionBox whileHover={{ y: -6, shadow: "lg" }} key={enArticleItem.id}>
+                    <Card
+                      key={enArticleItem.id}
                       title={enArticleItem.title}
                       colorMode={colorMode}
                     >
-                      {enArticleItem.readFrom.map((enArticleItemReadFrom, index) => (
-                        <NextLink key={index} href={enArticleItemReadFrom.articleLink} passHref>
+                      {enArticleItem.readFrom.map((enArticleItemReadFrom) => (
+                        <NextLink key={enArticleItemReadFrom.id} href={enArticleItemReadFrom.articleLink} passHref>
                           <Link isExternal>
                             <Tag size="sm" fontWeight="bold" m={["0px", "0px", "5px"]}>
                               Read from : {enArticleItemReadFrom.articleSource}
@@ -322,7 +202,7 @@ const About = ({ CompaniesCollection, EducationFacilities, PublishedArticlesColl
                           </Link>
                         </NextLink>
                       ))}
-                    </ArticleCard>
+                    </Card>
                   </MotionBox>
                 ))}
               </VStack>
@@ -344,16 +224,15 @@ const About = ({ CompaniesCollection, EducationFacilities, PublishedArticlesColl
                 mx={[0, 0, 6]}
                 mt={8}
               >
-                {PublishedArticlesCollectionPersian.map((faArticleItem, index) => (
-                  <MotionBox whileHover={{ y: -6, shadow: "lg" }} key={index}>
-                    <ArticleCard
-                      key={index}
+                {PersianPublishedArticlesData.map((faArticleItem) => (
+                  <MotionBox whileHover={{ y: -6, shadow: "lg" }} key={faArticleItem.id}>
+                    <Card
+                      key={faArticleItem.id}
                       title={faArticleItem.title}
-                      lang="fa"
                       colorMode={colorMode}
                     >
-                      {faArticleItem.readFrom.map((faArticleItemReadFrom, index) => (
-                        <NextLink key={index} href={faArticleItemReadFrom.articleLink} passHref>
+                      {faArticleItem.readFrom.map((faArticleItemReadFrom) => (
+                        <NextLink key={faArticleItemReadFrom.id} href={faArticleItemReadFrom.articleLink} passHref>
                           <Link isExternal>
                             <Tag size="sm" fontWeight="bold" m={["0px", "0px", "5px"]}>
                               Read from : {faArticleItemReadFrom.articleSource}
@@ -361,7 +240,7 @@ const About = ({ CompaniesCollection, EducationFacilities, PublishedArticlesColl
                           </Link>
                         </NextLink>
                       ))}
-                    </ArticleCard>
+                    </Card>
                   </MotionBox>
                 ))}
               </VStack>
@@ -376,10 +255,10 @@ const About = ({ CompaniesCollection, EducationFacilities, PublishedArticlesColl
 export function getStaticProps() {
   return {
     props: {
-      CompaniesCollection,
-      EducationFacilities,
-      PublishedArticlesCollectionEnglish,
-      PublishedArticlesCollectionPersian
+      CompaniesData,
+      EducationFacilitiesData,
+      EnglishPublishedArticlesData,
+      PersianPublishedArticlesData
     }
   };
 }
