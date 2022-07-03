954942fd35668872817114315014279043fb36a9
amirsina <amirsina23@gmail.com>
2022-07-03 15:01:01 +0430

    - refactor: rewrite technologies as an array of objects

--- data/independent-projects-list-data.ts
+++ data/independent-projects-list-data.ts
@@ -5,7 +5,20 @@ export const IndependentProjectsListData = [
     logo: '/assets/images/projects/midas.png',
     link: "https://mgl.cc/tr/",
     description: `Midas Global Logistics has been operating in international forwarding for over 15 years and was originally founded in 1996 in Istanbul.Midas Global Logistics’s activities cover all types of transports to and from any worldwide destination `,
-    technologies: ['Angularjs','Sass','Bootstrap',]
+    technologies: [
+      {
+        id: 9011,
+        name: 'Angularjs'
+      },
+      {
+        id: 9012,
+        name: 'Sass'
+      },
+      {
+        id: 9013,
+        name: 'Bootstrap'
+      },
+    ]
   },
   {
     id: 902,
@@ -13,7 +26,24 @@ export const IndependentProjectsListData = [
     logo: '/assets/images/projects/apex.png',
     link: "http://www.apexresource.us/",
     description: `Apex Resources Inc. is committed to maximizing the performance of most critical resources through development projects, as well as sourcing and distributing agricultural commodities, raw materials, medical devices, petroleum products, and LED lights.`,
-    technologies: ['Gulpjs','Sass','HTML5','Bootstrap']
+    technologies: [
+      {
+        id: 9021,
+        name: 'Gulpjs'
+      },
+      {
+        id: 9022,
+        name: 'Sass'
+      },
+      {
+        id: 9023,
+        name: 'HTML5'
+      },
+      {
+        id: 9024,
+        name: 'Bootstrap'
+      }
+    ]
   },
   {
     id: 903,
@@ -21,7 +51,24 @@ export const IndependentProjectsListData = [
     logo: '/assets/images/projects/ombs.png',
     link: "https://github.com/amirsinaa/OMBS",
     description: `OMBS is a solution to prevent drug wastage and expiration. It is a smart commercial platform for pharmacies around the world to buy and purchase drugs, especially drugs that are nearing expiration date.`,
-    technologies: ['Laravel','Vuejs','Sass','Bootstrap']
+    technologies: [
+      {
+        id: 9031,
+        name: 'Laravel'
+      },
+      {
+        id: 9032,
+        name: 'Vuejs'
+      },
+      {
+        id: 9033,
+        name: 'Sass'
+      },
+      {
+        id: 9034,
+        name: 'Bootstrap'
+      }
+    ]
   },
   {
     id: 904,
@@ -29,7 +76,20 @@ export const IndependentProjectsListData = [
     logo: '/assets/images/projects/Renaultyar.png',
     link: "https://renaultyar.com/",
     description: `Renaultyar (Land Forerunner Trade) has been established with the aim of facilitating the process of purchasing spare parts, providing original and standard parts and distinctive services, which results in increasing customer satisfaction.`,
-    technologies: ['Prestashop','Javascript Vanilla','Bootstrap']
+    technologies: [
+      {
+        id: 9041,
+        name: 'Prestashop'
+      },
+      {
+        id: 9042,
+        name: 'Vanilla Javascript'
+      },
+      {
+        id: 9043,
+        name: 'Bootstrap'
+      }
+    ]
   },
   {
     id: 905,
@@ -37,7 +97,20 @@ export const IndependentProjectsListData = [
     logo: '/assets/images/projects/irankoran.png',
     link: "http://irankoran.ir/",
     description: `irankoran project in 2017. , With the financial support of the German Ministry of Education, started working at the Berlin-Brandenburg Academy of Sciences. The main goal of the project is digital research and publication of manuscripts in the Quran that are kept in museums, libraries and public or private collections in Iran.`,
-    technologies: ['Yii2','Javascript Vanilla','Bootstrap']
+    technologies: [
+      {
+        id: 9051,
+        name: 'Yii2'
+      },
+      {
+        id: 9052,
+        name: ' Vanilla Javascript'
+      },
+      {
+        id: 9053,
+        name: 'Bootstrap'
+      }
+    ]
   },
   {
     id: 906,
@@ -45,7 +118,24 @@ export const IndependentProjectsListData = [
     logo: '/assets/images/projects/aca.png',
     link: "http://aca.ir/",
     description: `ACA company is a full-cycle software development company with niche expertise and a focus on the latest technologies. Since 2000, we have been helping Companies achieve their goals with impactful software solutions.`,
-    technologies: ['Gruntjs','Bootstrap','HTML5','Sass']
+    technologies: [
+      {
+        id: 9061,
+        name: 'Gruntjs'
+      },
+      {
+        id: 9062,
+        name: 'Bootstrap',
+      },
+      {
+        id: 9063,
+        name: 'HTML5'
+      },
+      {
+        id: 9064,
+        name: 'Sass'
+      }
+    ]
   },
   {
     id: 907,
@@ -53,6 +143,23 @@ export const IndependentProjectsListData = [
     logo: '/assets/images/projects/portfolio.png',
     link: "https://github.com/amirsinaa/amirsina.dev",
     description: `Personal portfolio built using React and Nextjs.`,
-    technologies: ['React','Nextjs','Typescript','ChakraUI']
+    technologies: [
+      {
+        id: 9071,
+        name: 'React'
+      },
+      {
+        id: 9072,
+        name: 'Nextjs'
+      },
+      {
+        id: 9073,
+        name: 'Typescript'
+      },
+      {
+        id: 9074,
+        name: 'ChakraUI'
+      },
+    ]
   },
 ];
