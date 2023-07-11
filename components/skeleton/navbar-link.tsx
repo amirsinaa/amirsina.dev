73b63193532cd8639e8c8e0636e439f3dbc608ba
amirsina <amirsina23@gmail.com>
2023-07-11 11:58:18 +0330

    -feature(new_version): improve navbar component ui performance

--- components/skeleton/navbar-link.tsx
+++ components/skeleton/navbar-link.tsx
@@ -1,24 +1,30 @@
 import {
-  useColorModeValue
+  useColorModeValue,
+  Text,
+  background
 } from '@chakra-ui/react'
 import Link from 'next/link'
 import { NavbarLinkProps } from '@/types/navbar'
 
-export default function NavbarLink (props: NavbarLinkProps) {
+export default function NavbarLink(props: NavbarLinkProps) {
   return (
     <Link
       href={props.path}
       onClick={() => props.onClose()}
-      // px={3}
-      // py={1.5}
-      // rounded={'md'}
-      // _hover={{
-      //   textDecoration: 'none',
-      //   bg: useColorModeValue('gray.200', 'gray.900'),
-      //   color: useColorModeValue('darkgreen.100', 'darkgreen.100')
-      // }}
     >
-      {props.name}
-    </Link>
+      <Text
+        bg='transparent'
+        as='span'
+        p={2}
+        borderRadius='2xl'
+        fontWeight='bold'
+        _hover={{
+          color: 'white',
+          background: useColorModeValue('gray.600', 'gray.600')
+        }}
+      >
+        {props.name}
+      </Text>
+    </Link >
   )
 }
--- components/skeleton/navbar-menu.tsx
+++ components/skeleton/navbar-menu.tsx
@@ -16,71 +16,69 @@ export default function NavbarMenu() {
   const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <Box
-      bg={useColorModeValue('white', 'gray.700')}
+      bg={useColorModeValue('deepBlueSea.100', 'gray.700')}
       px={4}
       boxShadow={'lg'}
       position='fixed'
       w='100%'
-      borderBottomLeftRadius='3xl'
-      borderBottomRightRadius='3xl'
       zIndex='999'
     >
-    <Flex
-      h={16}
-      alignItems={'center'}
-      justifyContent={'center'}
-      w={['100%', '95%', '90%', '85%', '80%', '75%', '70%']}
-      maxW={1200}
-      mx='auto'
-    >
-      <IconButton
-        size={'md'}
-        icon={isOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
-        aria-label={'Open Menu'}
-        display={['inherit', 'inherit', 'none']}
-        onClick={isOpen ? onClose : onOpen}
-      />
-      <HStack
-        spacing={8}
+      <Flex
+        h={16}
         alignItems={'center'}
-        justifyContent={'center'}>
-        <HStack
-          as={'nav'}
-          spacing={4}
-          display={{ base: 'none', md: 'flex' }}
-        >
-          {MenuLinks.map((link, index) => (
-            <NavbarLink
-              key={index}
-              name={link.name}
-              path={link.path}
-              onClose={onClose}
-            />
-          ))}
-        </HStack>
-      </HStack>
-    </Flex>
-
-    {isOpen ? (
-      <Box
-        pb={4}
-        w={['100%', '100%', '80%']}
+        justifyContent={'center'}
+        w={['100%', '95%', '90%', '85%', '80%', '75%', '70%']}
         maxW={1200}
-        display={['inherit', 'inherit', 'none']}
+        mx='auto'
       >
-        <Stack as={'nav'} spacing={4}>
-          {MenuLinks.map((link, index) => (
-            <NavbarLink
-              key={index}
-              index={index}
-              name={link.name}
-              path={link.path}
-              onClose={onClose}
-            />
-          ))}
-        </Stack>
-      </Box>
-    ) : null}
-  </Box>
+        <IconButton
+          size={'md'}
+          icon={isOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
+          aria-label={'Open Menu'}
+          display={['inherit', 'inherit', 'none']}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack
+          spacing={8}
+          alignItems={'center'}
+          justifyContent={'center'}>
+          <HStack
+            as={'nav'}
+            spacing={4}
+            display={{ base: 'none', md: 'flex' }}
+          >
+            {MenuLinks.map((link, index) => (
+              <NavbarLink
+                key={index}
+                name={link.name}
+                path={link.path}
+                onClose={onClose}
+              />
+            ))}
+          </HStack>
+        </HStack>
+      </Flex>
+
+      {isOpen ? (
+        <Box
+          pb={4}
+          w={['100%', '100%', '80%']}
+          maxW={1200}
+          display={['inherit', 'inherit', 'none']}
+        >
+          <Stack as={'nav'} spacing={4}>
+            {MenuLinks.map((link, index) => (
+              <NavbarLink
+                key={index}
+                index={index}
+                name={link.name}
+                path={link.path}
+                onClose={onClose}
+              />
+            ))}
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
   )
 }
