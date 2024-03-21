00efedd0b4f5386e586e100cfc2c7e75bc5420c8
Amirsina <amirsina23@gmail.com>
2024-03-21 20:38:47 +0330

    chore: upgrade project dependencies

---
+++ pages/_error.tsx
@@ -0,0 +1,13 @@
+import * as Sentry from '@sentry/nextjs'
+import Error from 'next/error'
+
+const CustomErrorComponent = (props) => {
+	return <Error statusCode={props.statusCode} />
+}
+
+CustomErrorComponent.getInitialProps = async (contextData) => {
+	await Sentry.captureUnderscoreErrorException(contextData)
+	return Error.getInitialProps(contextData)
+}
+
+export default CustomErrorComponent
